cbee99e1ef4bdf8a2f67545500db90ea6de9e2d8
Evanc09 <128946058+Evanc09@users.noreply.github.com>
2023-03-27 09:24:42 -0700

    Add Descriptions

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+#import modules 
 from time import *
 from tkinter import *
 
 
-
+# create tkinter application 
 root =  Tk()
 class funcs():
-    
-        
-        
-        # of the tkinter window
-    
+    #define functions 
     def timer():
+        #10 second timer 
         length = 10
         while True:
             sleep(1)
@@ -19,23 +17,27 @@ class funcs():
             if length == 0:
                 break
     def stopwatch():
-        
+        #stopwatch (runs infinitely)
         start = 0
         while True:
 
             sleep(1)
             start = start +1
             print(start)
-
+#create buttons
 Button(text="Timer(10 Seconds long)", command=lambda: [funcs.timer()]).pack()
 Button(text="Stopwatch(No Exit or pause function)", command= lambda:[funcs.stopwatch()]).pack()
 def time():
+    """current time function
+    Has to stay out of class or will cause errors"""
     string = strftime('%H:%M:%S %p')
     lbl.config(text=string)
     lbl.after(1000, time)
 lbl = Label(root, font=('calibri', 40, 'bold'),
             background='black',
             foreground='white')
+
 lbl.pack(anchor="center")
 time()
+#start the loop
 root.mainloop()
