b49e321f1cc1bdba18ae0c449bc3b70d58543118
Evanc09 <128946058+Evanc09@users.noreply.github.com>
2023-03-27 09:19:52 -0700

    Added Current time

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-import time
+from time import *
 from tkinter import *
 
 
 
 root =  Tk()
 class funcs():
-        def current_time():
-        string = strftime('%H:%M:%S %p')
-        lbl.config(text=string)
-        lbl.after(1000, time)
-        lbl = Label(root, font=('calibri', 40, 'bold'),
-                    background='black',
-                    foreground='white')
-
+    
+        
+        
         # of the tkinter window
-    lbl.pack(anchor='center')
+    
     def timer():
         length = 10
         while True:
-            time.sleep(1)
+            sleep(1)
             length = length -1
             print(f"Time left: {length} seconds")
             if length == 0:
@@ -28,11 +23,19 @@ class funcs():
         start = 0
         while True:
 
-            time.sleep(1)
+            sleep(1)
             start = start +1
             print(start)
-        
+
 Button(text="Timer(10 Seconds long)", command=lambda: [funcs.timer()]).pack()
 Button(text="Stopwatch(No Exit or pause function)", command= lambda:[funcs.stopwatch()]).pack()
-current_time()
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='black',
+            foreground='white')
+lbl.pack(anchor="center")
+time()
 root.mainloop()
