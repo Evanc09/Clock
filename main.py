27850df2a728db421e1442f38c2f057bdb172bab
Casey Carney <ecarns31@gmail.com>
2023-03-29 07:53:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from tkinter import *
 
 # create tkinter application 
 root =  Tk()
+root.resizable(0, 0)
+root.title("Clock")
+root.configure(bg="red")
 class funcs():
     #define functions 
     def timer():
@@ -15,22 +18,24 @@ class funcs():
             length = length -1
             print(f"Time left: {length} seconds")
             if length == 0:
+                print("Time's up#")
                 break
     def stopwatch():
         #stopwatch (runs infinitely)
         start = 0
         while True:
-
+            time()
             sleep(1)
             start = start +1
             print(start)
+
 #create buttons
 Button(text="Timer(10 Seconds long)", command=lambda: [funcs.timer()]).pack()
 Button(text="Stopwatch(No Exit or pause function)", command= lambda:[funcs.stopwatch()]).pack()
 def time():
     """current time function
     Has to stay out of class or will cause errors"""
-    string = strftime('%H:%M:%S %p')
+    string = strftime('%H:%M %p')
     lbl.config(text=string)
     lbl.after(1000, time)
 lbl = Label(root, font=('calibri', 40, 'bold'),
